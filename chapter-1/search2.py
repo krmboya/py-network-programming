537734409e24ce1c597ff37998a1adb704a22f9d
Kenny Rachuonyo <krmboya@gmail.com>
2015-03-03 16:52:21 +0300

    chapter-1/search2.py can now run under both python 2 and python 3

--- chapter-1/search2.py
+++ chapter-1/search2.py
@@ -1,13 +1,20 @@
 """
 Modified to use the Geocoding API v3. Uses file-like semantics
 """
-import urllib, urllib2
+
+try:
+    from urllib.parse import urlencode
+    from urllib.request import urlopen
+except ImportError:
+    from urllib import urlencode
+    from urllib2 import urlopen
 import json
 
 params = {"address": "207 N. Defiance St, Archbold, OH"}
-url = "http://maps.googleapis.com/maps/api/geocode/json?" + urllib.urlencode(params)
+url = "http://maps.googleapis.com/maps/api/geocode/json?" + urlencode(params)
 
-rawreply = urllib2.urlopen(url).read()
-reply = json.loads(rawreply)
+rawreply = urlopen(url).read()
+unicode_reply = rawreply.decode('utf-8')
+reply = json.loads(unicode_reply)
 location = reply["results"][0]["geometry"]["location"]
-print location.values()
+print(list(location.values()))
