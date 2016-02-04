058075100355369bd8aa325dd931c3b5f9901567
Kenny Rachuonyo <krmboya@gmail.com>
2016-02-04 12:33:01 +0300

    py3fy - big sender

--- chapter-2/big_sender.py
+++ chapter-2/big_sender.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Send a big UDP packet to our server
 # Demonstrates packet fragmentation
+from __future__ import print_function
 
 import IN, socket, sys
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -9,19 +10,22 @@ MAX = 65535
 PORT = 1060
 
 if len(sys.argv) != 2:
-    print >>sys.stderr, 'usage: big_sender.py host'
+    sys.stderr.write('usage: big_sender.py host\n')
     sys.exit(2)
 
 hostname = sys.argv[1]
 
 s.connect((hostname, PORT))
-s.setsockopt(socket.IPPROTO_IP, IN.IP_MTU_DISCOVER, IN.IP_PMTUDISC_DO)  # discover the mtu
+
+s.setsockopt(socket.IPPROTO_IP, 
+             IN.IP_MTU_DISCOVER, 
+             IN.IP_PMTUDISC_DO)  # discover the mtu
 
 try:
-    s.send("#" * 65000)
+    s.send("#".encode("utf-8") * 65000)
 except socket.error:
-    print 'The message did not make it'
+    print('The message did not make it')
     option = getattr(IN, "IP_MTU", 14)  # constant taken from <linux/in.h>
-    print "MTU:", s.getsockopt(socket.IPPROTO_IP, option)
+    print("MTU:", s.getsockopt(socket.IPPROTO_IP, option))
 else:
-    print 'The big message was sent'
+    print('The big message was sent')
