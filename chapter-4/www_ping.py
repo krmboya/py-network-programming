fe3358ba235532866768ac625b51adf0cbcd0516
Kenny Rachuonyo <krmboya@gmail.com>
2016-02-11 11:43:30 +0300

    py3fy www_ping.py

--- chapter-4/www_ping.py
+++ chapter-4/www_ping.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 # Find the www service of an arbitrary host using getaddrinfo
+from __future__ import print_function
 
 import socket
 import sys
 
 if len(sys.argv) != 2:
-    print >>sys.stderr, "usage: www_ping.py <hostname_or_ip>"
+    sys.stderr.write("usage: www_ping.py <hostname_or_ip>\n")
     sys.exit(1)
 
 hostname_or_ip = sys.argv[1]
@@ -17,7 +18,7 @@ try:
         socket.AI_ADDRCONFIG | socket.AI_V4MAPPED | socket.AI_CANONNAME)
 
 except socket.gaierror as e:
-    print "Name service failure:", e.args[1]
+    print("Name service failure: ", e.args[1])
     sys.exit(1)
 
 info = infolist[0]
@@ -27,6 +28,6 @@ s = socket.socket(*socket_args)
 try:
     s.connect(address)
 except socket.error as e:
-    print "Network failure:", e.args[1]
+    print("Network failure: ", e.args[1])
 else:
-    print "Success: host", info[3], 'is listening on port 80'
+    print("Success: host ", info[3], ' is listening on port 80')
