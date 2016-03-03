33116de87b4cca99a2e136c9027cb6c24b0583a8
Kenny Rachuonyo <krmboya@gmail.com>
2016-03-03 11:41:04 +0300

    py3fy blocks.py

--- chapter-5/blocks.py
+++ chapter-5/blocks.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Foundations of Python Network Programming - Chapter 5 - blocks.py
 # Sending data one block at a time.
+
+from __future__ import print_function
 
 import socket, struct, sys
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,7 +13,7 @@ PORT = 1060
 format = struct.Struct('!I')  # for messages up to 2**32 - 1 in length
 
 def recvall(sock, length):
-    data = ''
+    data = bytes()
     while len(data) < length:
         more = sock.recv(length - len(data))
         if not more:
@@ -22,24 +25,25 @@ def recvall(sock, length):
 def get(sock):
     lendata = recvall(sock, format.size)
     (length,) = format.unpack(lendata)
-    return recvall(sock, length)
+    return recvall(sock, length).decode("utf-8")
 
 def put(sock, message):
-    sock.send(format.pack(len(message)) + message)
+    bytes_ = message.encode("utf-8")
+    sock.send(format.pack(len(bytes_)) + bytes_)
 
 if sys.argv[1:] == ['server']:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen(1)
-    print 'Listening at', s.getsockname()
+    print('Listening at', s.getsockname())
     sc, sockname = s.accept()
-    print 'Accepted connection from', sockname
+    print('Accepted connection from', sockname)
     sc.shutdown(socket.SHUT_WR)  # only reading, no writing
     while True:
         message = get(sc)
         if not message:
             break
-        print 'Message says:', repr(message)
+        print('Message says:', repr(message))
     sc.close()
     s.close()
 
@@ -53,4 +57,4 @@ elif sys.argv[1:] == ['client']:
     s.close()
 
 else:
-    print >>sys.stderr, 'usage: blocks.py server|client [host]'
+    sys.stderr.write('usage: blocks.py server|client [host]\n')
