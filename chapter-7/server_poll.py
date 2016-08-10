7029c885dcc4917439d0f5bdb0b9741203fba535
Kenny Rachuonyo <krmboya@gmail.com>
2016-08-10 18:32:40 +0300

    some more annotations to the polling server

--- chapter-7/server_poll.py
+++ chapter-7/server_poll.py
@@ -21,8 +21,11 @@ while True:
         # For each socket file descriptor with an event to be handled
         
         sock = sockets[fd]
-        # Removed closed sockets from our list.
+
         if event & (select.POLLHUP | select.POLLERR | select.POLLNVAL):
+            # socket hung up, error or invalid requests condition occurred
+            # sock.close can trigger this
+            # clear info about socket
             poll.unregister(fd)
             del sockets[fd]
             requests.pop(sock, None)
@@ -43,22 +46,27 @@ while True:
         # Collect incoming data until it forms a question.
         elif event & select.POLLIN:
             data = sock.recv(4096)
-            if not data:      # end-of-file
+            if not data:      # received end-of-file
                 sock.close()  # makes POLLNVAL happen next time
                 continue
             requests[sock] += data
             if '?' in requests[sock]:
+                # Recieved a complete question
+                # Retrieve the answer and mark the socket for outgoing data
                 question = requests.pop(sock)
                 answer = dict(lancelot.qa)[question]
                 poll.modify(sock, select.POLLOUT)
-                responses[sock] = answer
+                responses[sock] = answer  # set the outgoing response
 
         # Send out pieces of each reply until they are all sent.
         elif event & select.POLLOUT:
+            # socket ready for outgoing data
             response = responses.pop(sock)
             n = sock.send(response)
             if n < len(response):
+                # not whole response sent, truncate to unsent
                 responses[sock] = response[n:]
             else:
+                # whole response sent, modify to expect incoming data
                 poll.modify(sock, select.POLLIN)
                 requests[sock] = ''
