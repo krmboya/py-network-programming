721b666b4e8d0c9acea7bc7793ba3ddb5c143be0
Kenny Rachuonyo <krmboya@gmail.com>
2016-09-08 17:40:56 +0300

    add the std lib SocketServer server

---
+++ chapter-7/server_SocketServer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Foundations of Python Network Programming - Chapter 7 - server_SocketServer.py
+# Answering Lancelot requests with a SocketServer.
+
+from SocketServer import ThreadingMixIn, TCPServer, BaseRequestHandler
+import lancelot, server_simple, socket
+
+class MyHandler(BaseRequestHandler):
+    def handle(self):
+        # self.request - the client socket
+        server_simple.handle_client(self.request)
+
+class MyServer(ThreadingMixIn, TCPServer):
+    """TCP server that starts a thread for each new client"""
+    allow_reuse_address = True
+    # address_family = socket.AF_INET6  # if you need IPv6
+
+server = MyServer(('', lancelot.PORT), MyHandler)
+server.serve_forever()
